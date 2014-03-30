9415b378e1cc6806f72ba446f697d8284b69a929
Troy <ttoggweiler2009@my.fit.edu>
2014-03-30 14:01:52 -0400

    woo ok now all my changes should be pushed

--- src/hint.js
+++ src/hint.js
@@ -4,7 +4,7 @@ function hint(){
         if(!check_Columns())
             if(!check_Rows())
                 if(!check_Boxs())
-                    if(false)//!row_Inspections())
+                    if(!row_Inspections())
                         alert("no hint available try checking to see if all values are correct");
                         
 }
@@ -112,10 +112,22 @@ function check_Box(column,row){
  
 //checks cells row column and box for valud value, returns on first found
 function row_Inspections(){
-    for(var j = 0; j < getGame().menu.user.length; j++){ // check each row
-        var missing_values = get_Missing_Row_Values(j); // find missing values for row
-        for(var n = 0; n < missing_values.length; n++){ // cycle through array 
-            check_Columns_X(missing_values[n]); //check each array element if it is only one of kind in column
+    for(var i = 0; i < getGame().menu.user.length; i++){
+        for(var j = 0; j < getGame().menu.user.length; j++){ // check each row
+            if(!getGame().given[i][j]){ // if cell is not a given
+                var row_Val = get_Missing_Row_Values(j); // find missing values for row
+                var col_Val = get_Missing_Column_Values(i); // find missing values for row
+                var box_Val = get_Missing_Box_Values(i,j); // find missing values for row
+                console.log("Row: "+row_Val);
+                console.log("Col: "+col_Val);
+                console.log("Box: "+box_Val);
+                var check = check_By_Inspection(row_Val,col_Val,box_Val);
+                if(check > 0){
+                    var msg = "Check this cells Row, Column and Box for a clue...\n";
+                    display_Hint(msg,i,j);
+                    return true;
+                }
+            }
         }
     }
     return false;
@@ -123,45 +135,46 @@ function row_Inspections(){
 
 //finds the numbers no currently in each row
 function get_Missing_Row_Values(row){
-    var missing_Values = [];
-    for(var n = 1; n <= 9; n++){
-        var found = false;
-        for(var j = 0; j < getGame().menu.user.length; j++){
-            if(getGame().menu.user[j][row] == n){
-                found = true;
-                break;
-            }
-        }
-        if(!found){
-            missing_Values.push(n)
-        }   
+    var gM = getGame().menu;
+    var row_Values = [false,false,false,false,false,false,false,false,false,false];
+    for(var i = 0; i < getGame().menu.user.length; i++){
+        row_Values[gM.user[i][row]] = true;
     }
-    console.log("not found:"+missing_Values);
-    return missing_Values;
+    return row_Values;
+}
+//finds the numbers no currently in each column
+function get_Missing_Column_Values(column){
+    var gM = getGame().menu;
+    var column_Values = [false,false,false,false,false,false,false,false,false,false];
+    for(var i = 0; i < getGame().menu.user.length; i++){
+        column_Values[gM.user[column][i]] = true;
+    }
+    return column_Values;
 }
 
-function check_Columns_X(myVal){
-    var fits = 0;
-    var column = null;
-    for(var i = 0; i < getGame().menu.user.length; i++){ //check each column
-        if(check_Column_X(myVal,i)){//if n fits only one column then we know it must go there
-            column = i;
-            fits++;
+//finds the numbers no currently in each box
+function get_Missing_Box_Values(column,row){
+    var gM = getGame().menu;
+    var box_Values = [false,false,false,false,false,false,false,false,false,false];
+    for(var y = 0; y < 3; y++){
+        for(var z = 0; z < 3; z++){
+            box_Values[gM.user[((Math.floor(column/3)*3)+y)][((Math.floor(row/3)*3)+z)]] = true;
         }
     }
-    if (fits > 1)
-        return 99;
-    return column;
+    return box_Values;     
 }
 
-function check_Column_X(myVal,i){
-    for(var j = 0; j < getGame().menu.user.length; j++){
-        if(getGame().menu.user[i][j] == myVal)
-            return false;
-    } 
-    return true;
-}
-
-function check_Box_X(){
-    return false;   
+//using the bitfields, check if 
+function check_By_Inspection(row_Val,col_Val,box_Val){
+    var value_fits = 0;
+    var value = 0;
+    for(var n = 1; n < row_Val.length; n++){
+        if(!row_Val[n] && !col_Val[n] && !box_Val[n]){ // checks for value that i
+            value = n;
+            value_fits++;
+            if(value_fits > 1)
+                return 0;
+        }
+    }
+    return value;
 }
