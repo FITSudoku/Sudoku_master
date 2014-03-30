f186cc1a31413d538b5dc448580e491877037695
alecbert <alec.bertossa@gmail.com>
2014-03-30 14:27:37 -0400

    First pass on submit
    My first changes to submit.php and index

--- src/submit.php
+++ src/submit.php
@@ -1,4 +1,5 @@
 <?php
+function submit() {
     $con = mysqli_connect("sudokuproject.mylha.com","edhjtylp_submit","SubmitData1!","edhjtylp_test");
    
     if (mysqli_connect_errno()) {
@@ -14,8 +15,8 @@
             $gender = get_post('gender');
             $education = get_post('education');
             $experience = get_post('experience');
-            $puzzleid = 1;
-            $timetaken = getCount();
+            $puzzleid = 3;
+            $timetaken = 22;
 
             mysqli_query($con, "INSERT INTO `Results`(`puzzleID`, `age`, `time taken`, `gender`, `education`, `experience`)
             VALUES('$puzzleid', '$age', '$timetaken', '$gender', '$education', '$experience');");
@@ -23,4 +24,5 @@
             mysqli_close($con);
         }
     }
+}
 ?>
