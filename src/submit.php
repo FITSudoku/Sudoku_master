4ce9b449336ce8cc6423415c2fd10753d45b14c1
alecbert <alec.bertossa@gmail.com>
2014-03-30 14:56:32 -0400

    Fixed submit.php
    No longer crashes silently

--- src/submit.php
+++ src/submit.php
@@ -1,28 +1,44 @@
+<html>
+<body>
 <?php
 function submit() {
+    echo "Started submit";
     $con = mysqli_connect("sudokuproject.mylha.com","edhjtylp_submit","SubmitData1!","edhjtylp_test");
-   
+    echo "Tried connection";
     if (mysqli_connect_errno()) {
             echo "Failed to connect to MySQL: " . mysqli_connect_error();
         } else {
-
-        if (isset($_POST[age]) &&
-            isset($_POST[gender]) &&
-            isset($_POST[education]) &&
-            isset($_POST[experience])) 
+        echo "Didn't fail connection";
+        if (isset($_POST['age']) &&
+            isset($_POST['gender']) &&
+            isset($_POST['education']) &&
+            isset($_POST['experience'])) 
         {
-            $age = get_post('age');
-            $gender = get_post('gender');
-            $education = get_post('education');
-            $experience = get_post('experience');
-            $puzzleid = 3;
+            echo "ISSET completed";
+            $age = $_POST['age'];
+            $gender = $_POST['gender'];
+            $education = $_POST['education'];
+            $experience = $_POST['experience'];
+            $puzzleid = 5;
             $timetaken = 22;
-
+            
             mysqli_query($con, "INSERT INTO `Results`(`puzzleID`, `age`, `time taken`, `gender`, `education`, `experience`)
             VALUES('$puzzleid', '$age', '$timetaken', '$gender', '$education', '$experience');");
 
             mysqli_close($con);
+            header('Location: http://sudokuproject.mylha.com/index.html');
+            exit();
+        } else {
+            echo "Could not find all data";
         }
     }
 }
+
+if(true)
+{
+   echo "Found true statement";
+   submit();
+}
 ?>
+</body>
+</html>
