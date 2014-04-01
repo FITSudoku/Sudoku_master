18804ddc1ac0610d9c306df08fbadebd82f80ee8
Troy <ttoggweiler2009@my.fit.edu>
2014-03-31 23:48:30 -0400

    almost lost all of this, puzzle arrays now in php

--- upload.php
+++ upload.php
@@ -1,12 +1,43 @@
-<!DOCTYPE html>
+<?php 
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+?>
 <html>
+    <script src="lib/jquery-2.1.0.min.js"></script>
 <body>
-
+    Hi, you are in upload.php!<br>
 <?php
-ECHO "Hello World!<br>";
-echo "Hello World!<br>";
-EcHo "Hello World!<br>";
-?>  
+function upload() {
+    echo "Started submit<br>";
+    $con = mysqli_connect("sudokuproject.mylha.com","edhjtylp_submit","SubmitData1!","edhjtylp_test");
+    echo "Tried connection<br>";
+    
+    $data=json_decode($_POST['upload']);
+    
+    foreach($data as $value){
+        echo "$value <br>";
+    }
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    } else if (false) {
+        echo "<br>Didn't fail connection<br>";
+        if (false) 
+        {
+            mysqli_query($con, "INSERT INTO `Results`(`puzzleID`, `age`, `time taken`, `gender`, `education`, `experience`)
+            VALUES('$puzzleid', '$age', '$timetaken', '$gender', '$education', '$experience');");
+            mysqli_close($con);
+            header('Location: http://sudokuproject.mylha.com/index.html');
+            exit();
+        } else {
+            echo "Could not find all data";
+        }
+    }
+}
 
+if(true)
+{
+   upload();
+}
+?>
 </body>
 </html>
