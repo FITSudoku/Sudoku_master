bf5a39e4512440ed4b6703b0f3a4bf881dfe86cb
alecbert <alec.bertossa@gmail.com>
2014-04-16 09:15:22 -0400

    Graphing set up
    Starting to set up some stuff for the graph. Kinda hoping it will just
    work.

--- makeGraph.js
+++ makeGraph.js
@@ -17,7 +17,45 @@ function drawGraph() {
         var obj = jsonObj[key];
         console.log(obj[document.getElementById("mainSort").value]);
     }
-    alert ("finished loop");
+    
+    //Creating svg reference
+    var svg = d3.select("body").append("svg")
+              .attr("width", width+margin.left+margin.right)
+              .attr("height", height+margin.top+margin.bottom)
+            .append("g")
+              .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+    //Creating the y scale for the dataset. Using a logarithmic function for now
+    var yScale = d3.scale.linear()
+                 .domain(0, d3.max(jsonObj, function(d) {
+                     return d["time taken"]
+                 }))
+                 .range([height, 0]);
+    
+    var formatAsInt = d3.format(",d");
+    
+    var yAxis = d3.svg.axis()
+                .scale(yScale)
+                .orient("left")
+                .tickFormat(formatAsInt);
+    
+    //Scale for sub attribute
+    var x0Scale = d3.scale.ordinal()
+                  .rangeRoundBands([0, width], 0.1);
+    
+    //Scale for main attribute
+    var x1Scale = d3.scale.ordinal();
+    
+    //Colors for the main attribute
+    var color = d3.scale.linear()
+                .range(["#aad", "#556"]);
+    
+    var xAxis = d3.svg.axis()
+                .scale(x0Scale)
+                .orient("bottom");
+    
+    
+    
 }
 
 function retrieveData() {
